65f4e8e179a2b89a85f3d4212fc5f126145ae6de
simc1 <simc1@lenovo.com>
2023-02-22 00:08:34 +0800

    Initialization

---
+++ gulimall-member/src/main/java/com/simc1/gulimall/Main.java
@@ -0,0 +1,12 @@
+package com.simc1.gulimall;
+
+/**
+ * Data
+ *
+ * @author simc1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
